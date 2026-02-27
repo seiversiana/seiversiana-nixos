86b03e1ee70ebc320651a8c4a75347dd97f994a2
Nile Jocson <seiversiana@gmail.com>
2026-02-28 00:02:02 +0800

    Disable password saving, extension install trigger, and default browser checks in firefox

--- seiversiana.nix
+++ seiversiana.nix
@@ -135,6 +135,9 @@ in {
     profiles.default = {
       isDefault = true;
       settings = {
+        "browser.shell.checkDefaultBrowser" = false;
+        "signon.rememberSignons" = false;
+        "extensions.InstallTrigger.enabled" = false;
         "extensions.autoDisableScopes" = 0;
       };
       extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
