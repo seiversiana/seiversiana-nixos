fedf1c632351c692094d0fb4d75f327c3e1fccb6
Nile Jocson <seiversiana@gmail.com>
2026-02-27 18:43:30 +0800

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,88 @@
+{ config, pkgs, ... }:
+
+{
+  system.stateVersion = "25.11";
+
+  imports = [
+      ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.hostName = "seiversiana-nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Manila";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  users.users.seiversiana = {
+    isNormalUser = true;
+    description = "Nile Jocson";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+ 
+  security.polkit.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    gcc
+    clang
+    clang-tools
+    cmake
+    vim
+    tmux
+    brightnessctl
+    lxsession
+  ];
+
+  programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+  };
+
+  services.openssh.enable = true;
+
+  services.xserver = {
+    enable = true;
+    displayManager.startx.enable = true;
+  };
+
+  services.displayManager.ly.enable = true;
+
+  programs.steam.enable = true;
+
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs; [ thunar-archive-plugin ];
+  };
+
+  services.gvfs.enable = true; 
+
+  nix.settings = {
+    substituters = [ "https://ezkea.cachix.org" ];
+    trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" ];
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "seiversiana-nixos flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    aagl = {
+      url = "github:ezKEa/aagl-gtk-on-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, nur, aagl, ... }: {
+    nixosConfigurations.seiversiana-nixos = nixpkgs.lib.nixosSystem {
+      modules = [ 
+        ./configuration.nix
+        home-manager.nixosModules.home-manager {
+          home-manager = {
+            backupFileExtension = "backup";
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.seiversiana = import ./seiversiana.nix;
+          };
+        }
+        nur.modules.nixos.default
+        {
+          imports = [ aagl.nixosModules.default ];
+          nix.settings = aagl.nixConfig;
+          programs.anime-game-launcher.enable = true; 
+        }
+      ];
+    };
+  };
+}
