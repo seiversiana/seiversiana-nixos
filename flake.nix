6961e9e496a5e103175117d5576d20ba231709cc
Nile Jocson <seiversiana@gmail.com>
2026-03-29 18:20:02 +0800

    Use unstable packages again

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
 	description = "seiversiana-nixos flake";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 		home-manager = {
-			url = "github:nix-community/home-manager/release-25.11";
+			url = "github:nix-community/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 		nur = {
@@ -12,7 +12,7 @@
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 		aagl = {
-			url = "github:ezKEa/aagl-gtk-on-nix/release-25.11";
+			url = "github:ezKEa/aagl-gtk-on-nix";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
